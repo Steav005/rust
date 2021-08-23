2273a6617cd28f3625b8306e6d4c1c131c7c4ee5
Autumnal <friedrich122112@me.com>
2021-08-23 20:03:57 +0200

    added l4re armv7 target

--- library/std/src/os/raw/mod.rs
+++ library/std/src/os/raw/mod.rs
@@ -61,7 +61,7 @@ type_alias! { "char.md", c_char = u8, NonZero_c_char = NonZeroU8;
         )
     ),
     all(target_os = "android", any(target_arch = "aarch64", target_arch = "arm")),
-    all(target_os = "l4re", target_arch = "x86_64"),
+    all(target_os = "l4re", any(target_arch = "x86_64", target_arch = "arm")),
     all(
         target_os = "freebsd",
         any(
@@ -103,7 +103,7 @@ type_alias! { "char.md", c_char = i8, NonZero_c_char = NonZeroI8;
         )
     ),
     all(target_os = "android", any(target_arch = "aarch64", target_arch = "arm")),
-    all(target_os = "l4re", target_arch = "x86_64"),
+    all(target_os = "l4re", any(target_arch = "x86_64", target_arch = "arm")),
     all(
         target_os = "freebsd",
         any(
